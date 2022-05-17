e8fce650aaa10c09a7fb2071f18ca5e503a7b403
semmzz <vladdis2000mn@gmail.com>
2022-05-18 00:07:10 +0300

    added container component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
 import {Routes, Route} from 'react-router-dom';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
 
@@ -21,9 +21,8 @@ const App = (props) => {
                         store={props.store}
                     />}
                     />
-                    <Route path={'/dialogs/*'} element={<Dialogs
-                        state={props.state.dialogsPage}
-                        dispatch={props.dispatch}
+                    <Route path={'/dialogs/*'} element={<DialogsContainer
+                        store={props.store}
                     />}
                     />
                     <Route path={'/news'} element={<News/>}/>
